b0b255745553ab63e5d2c2ab10186570b32d9178
amananand019 <64424809+amananand019@users.noreply.github.com>
2021-05-23 01:27:09 +0530

    setting navigation

---
+++ app/src/main/java/com/devil/premises/newsapp/ui/fragments/BreakingNewsFragment.kt
@@ -0,0 +1,7 @@
+package com.devil.premises.newsapp.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.devil.premises.newsapp.R
+
+class BreakingNewsFragment: Fragment(R.layout.fragment_breaking_news) {
+}
