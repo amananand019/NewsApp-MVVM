426f1c1f3537915a2252514f4b9ac71ece2cbd91
amananand019 <64424809+amananand019@users.noreply.github.com>
2021-05-28 23:26:24 +0530

    Dao and Article Setup

---
+++ app/src/main/java/com/devil/premises/newsapp/db/ArticleDao.kt
@@ -0,0 +1,17 @@
+package com.devil.premises.newsapp.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.devil.premises.newsapp.Article
+
+@Dao
+interface ArticleDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun upsert(article: Article): Long
+
+    @Query("SELECT * FROM articles")
+    fun getAllArticles(): LiveData<List<Article>>
+
+    @Delete
+    suspend fun deleteArticle(article: Article)
+}
